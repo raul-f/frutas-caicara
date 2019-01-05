f545c9edc6226479d650def25f6076a054f164f8
raul-f <raulf.dev@gmail.com>
2019-01-05 11:04:45 -0300

    Made folders more organized

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use('/public', express.static(path.join(__dirname, 'static')));
+app.use('/public', express.static(path.join(__dirname, '0-static')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'main/index.html'));
