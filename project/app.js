81b77ae6a14b4ffc1f5eef9b8306e032ed96a440
raul-f <raulf.dev@gmail.com>
2019-01-07 13:00:28 -0300

    added desing prototype

--- project/app.js
+++ project/app.js
@@ -7,19 +7,19 @@ const app = express();
 app.use('/public', express.static(path.join(__dirname, '0-static')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'main/index.html'));
+    res.sendFile(path.join(__dirname, '1-main/index.html'));
 });
 
 app.get('/quem-somos', (req, res) => {
-    res.sendFile(path.join(__dirname, 'quem-somos/index.html'));
+    res.sendFile(path.join(__dirname, '2-quem-somos/index.html'));
 });
 
 app.get('/onde-estamos', (req, res) => {
-    res.sendFile(path.join(__dirname, 'onde-estamos/index.html'));
+    res.sendFile(path.join(__dirname, '3-onde-estamos/index.html'));
 });
 
 app.get('/fale-conosco', (req, res) => {
-    res.sendFile(path.join(__dirname, 'fale-conosco/index.html'));
+    res.sendFile(path.join(__dirname, '4-fale-conosco/index.html'));
 });
 
 app.listen(process.env.PORT);
